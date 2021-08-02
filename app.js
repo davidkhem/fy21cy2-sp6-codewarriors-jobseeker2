519a0cdf61e5f5c079c65b5cf4e8443cc49fbd76
DSK978 <69495698+DSK978@users.noreply.github.com>
2021-08-01 21:58:28 -0400

    update html and js files

--- app.js
+++ app.js
@@ -9,64 +9,51 @@ const form = document.querySelector('#form');
 const API_KEY = 'f3949be83bb6df9c8ce6f1ed94bd50a4'; // ${API_KEY} need to set no-CORS first
 const API_URL = 'https://api.adzuna.com/v1/api/jobs/us/search/50?app_id=aed6ce79&app_key=f3949be83bb6df9c8ce6f1ed94bd50a4&results_per_page=10';
 
+const resultsDataSection = document.querySelector('#my-job-result')
+
 // API
 fetch(API_URL) // my promise
     .then(response => response.json())
     .then(result => {
-    const apiData = result.data
 
-    // create
-        const div = document.createElement('div')
-        div.innerHTML
+        const apiData = result.results;
+
+        apiData.forEach(aResult => {
+
+        // Start: create the single div (component)
+        const resultsInnerDiv = document.createElement('div')
+        resultsInnerDiv.innerHTML =
+            `<div class="w-1/3 h-28 flex flex-col justify-center ml-5 bg-blue-300">
+                   <h4 class="text-sm font-bold">${aResult['title']}</h4>
+                    <p class="text-sm">${aResult['company']['display_name']}</p>
+                    <p class="text-sm">${aResult['location']['display_name']}</p>
+                    <p class="text-sm mb-2">${aResult['created']}</p>
+            </div>`
+
+        resultsDataSection.appendChild(resultsInnerDiv)
+        // End component
+        })
+
         console.log(apiData);
     });
 
-
-
-
-
-
-
-// link to html page
-// const myResults = document.getElementById('my-job-result');
-//
-// myResults.forEach( item => {
-//     const divJobs = document.createElement('div')
-
-//     divJobs.innerHTML =
-//         `<div class="flex justify-center">
-//             <div class="w-1/3 h-52 flex flex-col flex justify-center">
-//                 <h4 class="text-sm font-bold">${item.title}</h4>
-//                 <p class="text-sm mb-2">${item.location.display_name}</p>
-//                 <p class="text-sm mb-2">${item.description}</p>
-//                 <p class="text-sm mb-4">${item.created}</p>
-//             </div>
-//     </div>`
-//
-//     apiData.appendChild(divJobs)
-//
-// });
-//
 // btn.addEventListener('click', () => {
-//     myResults.classList.remove('hidden');
+//     resultsDataSection.classList.remove('hidden');
 // });
-
-
 
 // event listener
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-
-});
-
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//
+//
+// });
 
 // adding alert
-function validate() {
-    if(city.value.trim() === '') {
-        alert('Please enter City/Town');
-        return false;
-    } else {
-        return true;
-    }
-}
+// function validate() {
+//     if(city.value.trim() === '') {
+//         alert('Please enter City/Town');
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
